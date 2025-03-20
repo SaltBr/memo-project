d8bb38b10c11fc7cf87a465feb7df815d0bfafb6
SaltBr <snowgriff123@gmail.com>
2025-03-20 14:54:26 +0900

    Feat: 추가 기능
    - 전체 조회 기능 추가
    - 제목만 수정하는 기능 추가
    - ResponseEntity로 변경

--- src/main/java/org/example/memo/controller/MemoController.java
+++ src/main/java/org/example/memo/controller/MemoController.java
@@ -3,11 +3,11 @@ package org.example.memo.controller;
 import org.example.memo.dto.MemoRequestDto;
 import org.example.memo.dto.MemoResponseDto;
 import org.example.memo.entity.Memo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/memos")
@@ -17,7 +17,7 @@ public class MemoController {
     private final Map<Long, Memo> memoList = new HashMap<>();
 
     @PostMapping
-    public MemoResponseDto createMemo(@RequestBody MemoRequestDto dto) {
+    public ResponseEntity<MemoResponseDto> createMemo(@RequestBody MemoRequestDto dto) {
         //식별자가 1씩 증가하도록
         //값이 없다면 1, 값이 있다면 그중 가장 큰 값 + 1
         Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
@@ -29,29 +29,85 @@ public class MemoController {
         memoList.put(memoId, memo);
 
         //생성된 메모(하나)를 클라에 리턴
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MemoResponseDto>> findAllMemos() {
+        //init list
+        List<MemoResponseDto> responseList = new ArrayList<>();
+
+        //HashMap<Memo> -> List<MemoResponseDto>
+        for(Memo memo: memoList.values()){
+            MemoResponseDto responseDto = new MemoResponseDto(memo);
+            responseList.add(responseDto);
+        }
+
+        //Map to List
+        //responseList = memoList.values().stream().map(MemoResponseDto::new).toList();
+
+        return new ResponseEntity<>(responseList, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public MemoResponseDto findMemoById(@PathVariable Long id) {
+    public ResponseEntity<MemoResponseDto> findMemoById(@PathVariable Long id) {
         Memo memo = memoList.get(id);
-        return new MemoResponseDto(memo);
+        if(memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public MemoResponseDto updateMemoById(
+    public ResponseEntity<MemoResponseDto> updateMemoById(
             @PathVariable Long id,
             @RequestBody MemoRequestDto dto
     ) {
         Memo memo = memoList.get(id);
 
+        if(memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        if(dto.getTitle() == null || dto.getContents()==null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
         memo.update(dto);
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<MemoResponseDto> updateTitle(
+            @PathVariable Long id,
+            @RequestBody MemoRequestDto dto
+    ) {
+        Memo memo = memoList.get(id);
+        //NPE 방지
+        if(memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        if(dto.getTitle() == null || dto.getContents()!=null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        memo.updateTitle(dto);
+
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteMemoById(@PathVariable Long id) {
-        memoList.remove(id);
+    public ResponseEntity<Void> deleteMemoById(@PathVariable Long id) {
+        //ResponseEntity 안에 넣는 보이드는 대문자 보이드
+
+        //memoList의 key값에 id를 포함하고 있다면
+        if(memoList.containsKey(id)){
+            memoList.remove(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        return new ResponseEntity<> (HttpStatus.NOT_FOUND);
     }
 
 }
